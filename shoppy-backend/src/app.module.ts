b4d5ac8b4ff37709950aa3b2ee8e31752365ca1c
taofeeekishola <taofeekishola1791@gmail.com>
2026-02-22 15:44:12 +0100

    created prisma module and service

--- shoppy-backend/src/app.module.ts
+++ shoppy-backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
 
+
 @Module({
   imports: [
     LoggerModule.forRootAsync({
--- shoppy-backend/src/users/users.service.ts
+++ shoppy-backend/src/users/users.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt'
 import { CreateuserRequest } from './dto/create-user.request';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from 'generated/prisma/client';
 
 @Injectable()
 export class UsersService {
-    createUser(data: CreateuserRequest){
-        console.log(data);
+    constructor (private readonly prismaService: PrismaService){}
+
+
+    async createUser(data: CreateuserRequest){
+        try{
+            return await this.prismaService.user.create({
+                data: {
+                    ...data,
+                    password: await bcrypt.hash(data.password, 10),
+                },
+                select:{
+                    email: true,
+                    id: true,
+                }
+            });
+        } catch (err){
+            console.error(err);
+            if(err.code === 'P2002'){
+                throw new UnprocessableEntityException('Email already exists');
+            }
+            throw err;
+        }
     }
 }
